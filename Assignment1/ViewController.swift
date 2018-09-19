b12a6fa64f87b5f517838eb1c40c7c5a47575fd7
tomo <tsutlovic@gmail.com>
2018-09-19 14:38:07 -0400

    Initial Commit

---
+++ Assignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by Thomas Sutlovic on 2018-09-19.
+//  Copyright © 2018 ThomasSutlovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
